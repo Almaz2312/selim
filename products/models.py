55687f0ff468e39c325771bd91a5206b9a731926
azamat-medenov <medenovvaza@gmail.com>
2024-02-02 18:04:11 +0600

    defined "Feedback", "OurWorks", "Product", "Category" models

--- products/models.py
+++ products/models.py
@@ -1,1 +1,33 @@
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="category")
+    description = models.TextField()
+
+
+class Product(models.Model):
+    type = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to="products")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.CASCADE,
+        related_name="product",
+    )
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="feedbacks")
+    body = models.TextField()
+
+
+class OurWorks(models.Model):
+    image = models.ImageField(upload_to="uploads")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
