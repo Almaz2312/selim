274f969cab6ec6536fbde29019dbafd3d3413496
azamat-medenov <medenovvaza@gmail.com>
2024-02-04 09:29:50 +0600

    created ProductByCategory view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -8,3 +10,9 @@ class ProductListView(ListAPIView):
     model = Product
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+
+class ProductByCategory(APIView):
+    def get(self, request, category):
+        p = Product.objects.filter(category__name=category)
+        return Response(ProductSerializer(p, many=True).data)
