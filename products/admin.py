9fd375b6fe8177303c6b9ace956438795cf7f0ef
azamat-medenov <medenovvaza@gmail.com>
2024-02-12 13:07:39 +0600

    created admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Feedback, OurWorks, Product, ProductImage
+
+
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OurWorks)
+class OurWorksAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
