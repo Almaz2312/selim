f9930f3078b52b35e479fd6305a5cb93d704046d
azamat-medenov <medenovvaza@gmail.com>
2024-02-15 18:15:19 +0600

    changed ProductListView by django-filter

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
 
-from .views import (
-    CategoryListView,
-    FeedbackListView,
-    ProductByCategory,
-    ProductListView,
-)
+from .views import CategoryListView, FeedbackListView, ProductListView
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
     path("feedbacks/", FeedbackListView.as_view(), name="feedbacks"),
     path("categories/", CategoryListView.as_view(), name="categories_list"),
-    path("<str:category>/", ProductByCategory.as_view(), name="product_by_category"),
 ]
